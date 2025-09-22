506a3a4a5f2ec0f56b2c763046e074ac9c23ba48
Weaver17 <ajweaver18@gmail.com>
2025-09-22 00:12:21 -0400

    feat: search page

--- src/app/(client)/all-games/page.tsx
+++ src/app/(client)/all-games/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function page() {
-    return <div>page</div>;
+function AllGamesPage() {
+    return <div>AllGamesPage</div>;
 }
 
-export default page;
+export default AllGamesPage;
