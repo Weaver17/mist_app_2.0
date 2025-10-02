7bdcab06ed7296ec8d28bec1d861fa9298293a25
Weaver17 <ajweaver18@gmail.com>
2025-10-02 14:27:19 -0400

    feat: about

--- src/app/(client)/about/page.tsx
+++ src/app/(client)/about/page.tsx
@@ -1,5 +1,9 @@
 import { CustomButton } from "@/components/custom/c_button";
-import { CustomCard, CustomCardContent } from "@/components/custom/c_card";
+import {
+    CustomCard,
+    CustomCardContent,
+    CustomCardHeader,
+} from "@/components/custom/c_card";
 import FadingSidebar from "@/components/profile/fading-sidebar";
 import { H1Custom, H3Custom, PCustom } from "@/typography/custom";
 import Link from "next/link";
@@ -18,11 +22,21 @@ function AboutPage() {
                             My Saved Games:
                         </H3Custom>
                         <ul className="space-y-4 text-center font-special whitespace-nowrap">
-                            <li>Marvel Heroes</li>
-                            <li>Frag Punk</li>
-                            <li>League of Legends</li>
-                            <li>Overwatch 2</li>
-                            <li>Smite</li>
+                            <li>
+                                <Link href="/game/599">Marvel Rivals</Link>
+                            </li>
+                            <li>
+                                <Link href="/game/604">Frag Punk</Link>
+                            </li>
+                            <li>
+                                <Link href="/game/286">League of Legends</Link>
+                            </li>
+                            <li>
+                                <Link href="/game/540">Overwatch 2</Link>
+                            </li>
+                            <li>
+                                <Link href="/game/217">Smite</Link>
+                            </li>
                         </ul>
                     </FadingSidebar>
                 </div>
@@ -31,33 +45,48 @@ function AboutPage() {
                         <CustomCardContent>
                             <div className="flex flex-col pb-2 gap-4 border-b border-primary md:flex-row md:justify-end">
                                 <CustomButton asChild variant="link">
-                                    <Link href="/">GitHub</Link>
+                                    <Link
+                                        href="https://github.com/Weaver17"
+                                        target="_blank"
+                                    >
+                                        GitHub
+                                    </Link>
                                 </CustomButton>
                                 <CustomButton asChild variant="link">
-                                    <Link href="/">LinkedIn</Link>
+                                    <Link
+                                        href="https://www.linkedin.com/in/andrew-weaver-1725-profile/"
+                                        target="_blank"
+                                    >
+                                        LinkedIn
+                                    </Link>
                                 </CustomButton>
                                 <CustomButton asChild variant="link">
-                                    <Link href="/">Elevate Auto Detailing</Link>
+                                    <Link
+                                        href="https://elevate-auto-detailing.com/"
+                                        target="_blank"
+                                    >
+                                        Elevate Auto Detailing
+                                    </Link>
                                 </CustomButton>
                             </div>
                             <PCustom>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Quasi maxime cupiditate
-                                aspernatur iste sapiente, rerum, nulla nisi
-                                facere laudantium optio recusandae ab sit ad
-                                reiciendis odio illo neque sequi explicabo
-                                doloremque iure consequuntur, adipisci
-                                provident! Repellat illum eius autem libero,
-                                eveniet adipisci, ipsa voluptate voluptatibus
-                                consequatur labore eaque. Esse sint dolore
-                                libero. Expedita quam dolorum amet dolorem, illo
-                                minus inventore ipsum impedit, excepturi tempore
-                                similique cum ullam a praesentium dignissimos
-                                quidem consequatur mollitia cupiditate! Ea
-                                magnam, officiis dicta suscipit quos molestias
-                                inventore molestiae tempora, amet accusamus
-                                alias at, officia adipisci voluptatem. Esse quam
-                                libero illum dolorem, voluptate amet quos ut?
+                                Hello there! My name is Andrew Weaver and I
+                                originally created this web page for a final
+                                project for TripleTen&apos;s Full Stack
+                                Engineering bootcamp. They had me use Node.js
+                                and Vite + React to lauch it on the Google
+                                Cloud. Since graduating, I learned Typescript
+                                and Next.js and also starting using Prisma ORM
+                                and shadcn components. I enjoyed them and
+                                decided to port my MIST site over to Next.js and
+                                Vercel. I did some styling updates and
+                                implemented BetterAuth, but the site is mostly
+                                the same, so it only took a week or two, and
+                                only a tiny bit of help from AI. The links above
+                                take you to my GitHub and LinkedIn pages and I
+                                also own my own car detailing business, so if
+                                anyone is around Columbus, Ohio, feel free to
+                                check it out!
                             </PCustom>
                         </CustomCardContent>
                     </CustomCard>
@@ -66,23 +95,30 @@ function AboutPage() {
             <div className="flex flex-col gap-4 md:px-12">
                 <CustomCard>
                     <CustomCardContent>
+                        <CustomCardHeader className="py-4 border-b border-primary">
+                            <CustomButton asChild variant="link">
+                                <Link
+                                    href="https://www.freetogame.com/"
+                                    target="_blank"
+                                >
+                                    FreeToGame.com
+                                </Link>
+                            </CustomButton>
+                        </CustomCardHeader>
                         <PCustom>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Quasi maxime cupiditate aspernatur iste
-                            sapiente, rerum, nulla nisi facere laudantium optio
-                            recusandae ab sit ad reiciendis odio illo neque
-                            sequi explicabo doloremque iure consequuntur,
-                            adipisci provident! Repellat illum eius autem
-                            libero, eveniet adipisci, ipsa voluptate
-                            voluptatibus consequatur labore eaque. Esse sint
-                            dolore libero. Expedita quam dolorum amet dolorem,
-                            illo minus inventore ipsum impedit, excepturi
-                            tempore similique cum ullam a praesentium
-                            dignissimos quidem consequatur mollitia cupiditate!
-                            Ea magnam, officiis dicta suscipit quos molestias
-                            inventore molestiae tempora, amet accusamus alias
-                            at, officia adipisci voluptatem. Esse quam libero
-                            illum dolorem, voluptate amet quos ut?
+                            FreeToGame.com is essentially this webpage except it
+                            is way better in every way, but this one is nice,
+                            too. It is is a gaming platform that brings together
+                            the best Free-to-Play Multiplayer Online Games and
+                            MMO Games into one convenient place. Whether
+                            you&apos;re into MMORPGs, MOBAs, FPS, or strategy
+                            games, FreeToGame offers a comprehensive gaming
+                            experience tailored to your interests. FreeToGame
+                            also offers a free public API that can be accessed
+                            without any restrictions or the need to create an
+                            account. This Free-To-Play Games Database API
+                            provides developers and enthusiasts with valuable
+                            data to enhance their gaming projects.
                         </PCustom>
                     </CustomCardContent>
                 </CustomCard>
