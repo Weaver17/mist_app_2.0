83bad2b37ba312b4de501bc79e07686c39217909
Weaver17 <ajweaver18@gmail.com>
2025-10-03 13:07:28 -0400

    fix: vercel db connection

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,4 +1,10 @@
 import { createAuthClient } from "better-auth/react";
+
+const BASE_URL =
+    process.env.NODE_ENV === "production"
+        ? "https://mistfreegames.vercel.app/"
+        : "http://localhost:3000/";
+
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: BASE_URL,
 });
