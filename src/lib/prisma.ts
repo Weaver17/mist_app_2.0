a4e7df1b6d5546fb3eadf475443aa1969d10ec92
Weaver17 <ajweaver18@gmail.com>
2025-10-03 13:56:03 -0400

    fix: vercel db connection 5

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,39 +2,23 @@
 
 import { PrismaClient } from "../../generated/prisma-client";
 
-// const connectionString =
-//     process.env.NODE_ENV === "production"
-//         ? process.env.DATABASE_URL!
-//         : process.env.MIST_SQL_DB_PRISMA_DATABASE_URL!;
+const connectionString =
+    process.env.NODE_ENV === "production"
+        ? process.env.DATABASE_URL
+        : process.env.MIST_SQL_DB_PRISMA_DATABASE_URL;
 
-let globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-let prisma = new PrismaClient({
-    datasources: {
-        db: {
-            url: process.env.MIST_SQL_DB_PRISMA_DATABASE_URL,
-        },
-    },
-});
-
-if (typeof window === "undefined") {
-    if (process.env.NODE_ENV === "production") {
-        prisma = new PrismaClient({
-            datasources: {
-                db: {
-                    url: process.env.DATABASE_URL,
-                },
+const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        datasources: {
+            db: {
+                url: connectionString,
             },
-        });
-    } else {
-        if (!globalForPrisma) {
-            globalForPrisma = global as unknown as { prisma: PrismaClient };
-        }
+        },
+    });
 
-        prisma = globalForPrisma.prisma || (globalForPrisma.prisma = prisma);
-    }
-} else {
-    throw new Error("Attempt to use prisma in client");
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
